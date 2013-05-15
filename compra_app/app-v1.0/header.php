3004bbf2dc08e92f1e4c34e7514dacf907f0bb89
Catt Small <cattsmall@gmail.com>
2013-05-15 13:39:07 -0400

    Basic skeleton done
    I can leave it alone now. Phew
    
    Signed-off-by: Catt Small <cattsmall@gmail.com>

--- compra_app/app-v1.0/header.php
+++ compra_app/app-v1.0/header.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" href="lib/css/styles.css" type="text/css" media="screen" charset="utf-8">
+		<link rel="stylesheet" href="lib/css/print.css" type="text/css" media="print" charset="utf-8">
+		<script src="lib/js/lib/jquery-2.0.0.min.js" type="text/javascript" charset="utf-8"></script>
 	</head>
 	<body>
+		<header>
+			<nav class="navigation">
+				<h2><a class="home-link" href="index.php">Compra</a></h2>
+				<ul>
+					<li class="settings-link"><a href="preferences.php"><i class="icon-user"></i></a></li>
+				</ul>
+			</nav>
+			<?php include('forms/add-task.php'); ?>
+		</header>
---
+++ compra_app/app-v1.0/index.php
@@ -0,0 +1,28 @@
+<?php include('header.php'); ?>
+<ul class="task-list">
+	<li class="task">
+		<section class="task-info">
+			<h3 class="task-title">Task name</h3>
+			<p>Task notes if they want</p>
+			<form class="task-price-range">
+				<select class="task-price-currency">
+					<option>$</option>
+					<option>£</option>
+				</select>
+				<fieldset class="task-price minimum">
+					<label>Min:</label>
+					<input type="number" placeholder="$0">
+				</fieldset>
+				<fieldset class="task-price maximum">
+					<label>Min:</label>
+					<input type="number" placeholder="$0">
+				</fieldset>
+				<button class="submit save-task"><i class="icon-ok"></i></button>
+			</form>
+		</section>
+		<aside class="delete-task">
+			<i class="icon-mail-reply"></i> <h4>Continue sliding to delete</h4>
+		</aside>
+	</li>
+</ul>
+<?php include('footer.php'); ?>
