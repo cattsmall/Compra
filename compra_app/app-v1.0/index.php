8693e7bbbbf3fac54b304172fc0f1289860d43a5
Catt Small <cattsmall@gmail.com>
2013-06-23 12:41:38 -0400

    Additions to variables and list items

--- compra_app/app-v1.0/index.php
+++ compra_app/app-v1.0/index.php
@@ -1,11 +1,16 @@
 <?php include('header.php'); ?>
 <ul class="task-list">
 	<li class="task">
-		<section class="task-info">
+		<article class="task-info">
 			<header class="task-text">
-				<h3 class="task-title">Task name</h3>
-				<p>Task notes if they want</p>
+				<h3 class="task-title" contenteditable="true">Task name</h3>
+				<p contenteditable="true">Task notes if they want</p>
 			</header>
+			<section class="task-price-range">
+				<p class="task-price minimum"><strong>Min:</strong> <span class="price">$0</span></p>
+				<p class="task-price maximum"><strong>Max:</strong> <span class="price">$0</span></p>
+				<a class="edit-task" href="#"><i class="icon-pencil"></i></a>
+			</section>
 			<form class="task-price-range">
 				<select class="task-price-currency">
 					<option>$</option>
@@ -17,13 +22,13 @@
 						<input type="number" placeholder="$0">
 					</fieldset>
 					<fieldset class="task-price maximum">
-						<label>Min:</label>
+						<label>Max:</label>
 						<input type="number" placeholder="$0">
 					</fieldset>
 				</div>
 				<button class="submit save-task"><i class="icon-ok"></i></button>
 			</form>
-		</section>
+		</article>
 		<aside class="delete-task">
 			<i class="icon-mail-reply"></i> <h4>Continue sliding to delete</h4>
 		</aside>
