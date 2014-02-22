ee054c75fc4ae392d2e57f9c62cb2eb6bd3a0116
Catt Small <cattsmall@gmail.com>
2014-02-22 13:59:56 -0500

    Starting over with Angular -- we'll see how it goes!

--- compra_app/app-v1.0/forms/add-task.php
+++
@@ -1,4 +0,0 @@
-<form class="add-task">
-	<input type="text" placeholder="Enter an item you'd like to buy">
-	<button class="add-task-button submit"><i class="icon-plus"></i></button>
-</form>
--- compra_app/app/lib/js/app-ck.js
+++
@@ -1,1 +0,0 @@
-if(Meteor.isClient){Template.hello.greeting=function(){return"Welcome to app."};Template.hello.events({"click input":function(){typeof console!="undefined"&&console.log("You pressed the button")}})}Meteor.isServer&&Meteor.startup(function(){});
